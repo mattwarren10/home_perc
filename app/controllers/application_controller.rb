45c7aeb03e61e4676632294d5a8f71cb75405f61
Matt Warren <mwarren@harding.edu>
2016-09-23 17:08:13 -0400

    enable log in and log out with form_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :permit_search
   protected
   def configure_permitted_parameters
   	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def permit_search
+  	@search = Search.new()
   end
 end
