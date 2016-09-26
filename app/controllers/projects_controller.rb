e89c779040d3150617cb3ef67e320c76ce32584d
Matt Warren <mwarren@harding.edu>
2016-09-26 17:35:25 -0400

    add ability to add prices to total_cost

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,21 +7,21 @@ class ProjectsController < ApplicationController
 		@project = Project.find(params[:id])
 	end
 
+	def new
+		@project = Project.new
+	end
+
 	def add_item
 		project = Project.find_by(id: params[:project_id])
 		product = Product.new(
 			photo: params[:product][:photo],
 			description: params[:product][:description],
-			price: params[:product][:price],
+			price: (params[:product][:price].match(/\d+/)[0].to_f / 100),
 			merchant: params[:product][:merchant]
 		)
 		project.products.push( product )
 		project.total_price += product.price
 		project.save
 		flash[:success] = "Item added!"
-	end
-
-	def find_params 
-
 	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
 	has_many :project_products
 	has_many :projects, through: :project_products
+	validates :price, numericality: {:greater_than => 0}
 end
